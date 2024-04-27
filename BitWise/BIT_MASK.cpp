894216a6506fbc5ed2cda658ff147aced241443a
Taufiqeelahi <1811taufiqeelahi@gmail.com>
2024-04-27 15:45:46 +0600

    First Commit

--- BitWise/BIT_MASK.cpp
+++ BitWise/BIT_MASK.cpp
@@ -2,24 +2,19 @@
 
 using namespace std; 
 
-int main()
-{
+int main() {
     int n ;
     cin>>n ;
 
     int arr[n] ;
 
-    for(int i = 0 ; i<n ; i++)
-    {
+    for(int i = 0 ; i<n ; i++) {
         cin>>arr[i] ;
     }
 
-    for(int mask = 0 ; mask<=(1<<n) ;mask++)
-    {
-        for(int i =0 ; i<n; i++)
-        {
-            if((mask>>i)&1)
-            {
+    for(int mask = 0 ; mask<=(1<<n) ;mask++) {
+        for(int i =0 ; i<n; i++)  {
+            if((mask>>i)&1)  {
                 cout<<" "<<arr[i]<<" " ;
             }
         }

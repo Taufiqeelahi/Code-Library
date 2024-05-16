d1be34c12c1c3a0d33db3d98f08c8be1cd5070bc
Taufiqeelahi <1811taufiqeelahi@gmail.com>
2024-05-16 18:18:40 +0600

    Modified

--- Number Theory/prime_factor_using_sieve.cpp
+++ Number Theory/prime_factor_using_sieve.cpp
@@ -1,41 +1,30 @@
 #include<bits/stdc++.h>
-#define endl '\n'
-#define FAST_IO ios_base::sync_with_stdio(0) ; cin.tie(0) ; cout.tie(0) ;
-typedef long long int ll ;
-
 using namespace std ;
-
 
 const int N = 1e6 + 9 ;
 int spf[N] ;
-int main()
-{
-    FAST_IO ;
-    for(int i = 2 ; i<N ; i++)
-    {
-        spf[i] = i ;  
+int main() {
+    ios_base::sync_with_stdio(0) ;
+    cin.tie(0) ;
+    for(int i = 2 ; i<N ; i++) {
+        spf[i] = i ;
     }
 
-    for(int i = 2 ; i<N ;i++)
-    {
-        for(int j = i ; j<N ; j += i)
-        {
-            spf[j] = min(spf[j],spf[i]);
+    for(int i = 2 ; i<N ; i++) {
+        for(int j = i ; j<N ;j += i) {
+            spf[j] = min(spf[j],i) ;
         }
     }
 
-    int q ; cin>>q ;
-    while(q--)
-    {
-        int n ; cin>>n ;
+    int q ; cin >> q ; // Queries q <= 1e6
+    while(q--) {
+        int n ; cin >> n ; // n <= 1e6 
         vector<int>ans ;
-
-        while(n>1)
-        {
-            ans.push_back(spf[n]);
+        while(n>1) {
+            ans.push_back(spf[n]) ;
             n /= spf[n] ;
         }
-        for(auto x:ans ) cout<<x<<" ";
-        cout<<endl;
+        for(auto u:ans) cout<<u<<' ';
+        cout<<'\n' ;
     }
 }

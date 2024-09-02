9081855ebca47ec10ad43684d32b8698b654ae40
Taufiqeelahi <1811taufiqeelahi@gmail.com>
2024-09-02 07:44:13 +0600

    Added

--- Graph/Connected_Component_using_DFS.cpp
+++ Graph/Connected_Component_using_DFS.cpp
@@ -1,33 +1,52 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std ;
 
-const int N = 1e5 + 9 ;
-vector<int> g[N];
+const int N = 1e5 ;
+vector<int>g[N] ;
 bool vis[N] ;
-void dfs( int u) {
-    vis[u] = true  ;
-    for(auto v:g[u]) {
-        if(!vis[v]) {
-            dfs(v) ;
-        }
-    }
+
+vector<vector<int>>connected_component ;
+vector<int>current_connected_component ;
+void dfs(int root) {
+  vis[root] = true ;
+  current_connected_component.push_back(root) ;
+  for(int children:g[root]) {
+    if(!vis[children]) dfs(children) ;
+  }
 }
+
 int32_t main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  int n, m; cin >> n >> m;
-  while (m--) {
-    int u, v; cin >> u >> v;
-    g[u].push_back(v);
-    g[v].push_back(u);
+  ios_base::sync_with_stdio(0) ;
+  cin.tie(0) ;
+
+  int n , m ; cin >> n >> m ; 
+  for(int i = 0 ; i < m ; i++) {
+    int a , b ; cin >> a >> b ;
+    g[a].push_back(b) ;
+    g[b].push_back(a) ;
+  }
+
+  // Connected Coponents
+
+  int cnt = 0 ;
+
+  for(int i = 1 ;  i <= n ; i++) {
+    if(vis[i]) continue ;
+    current_connected_component.clear() ;
+    dfs(i) ;
+    connected_component.push_back(current_connected_component) ;
+    cnt++ ;
   }
   
-  int ans = 0 ;
-  for(int i = 1 ; i <= n ; i++) {
-    if(!vis[i]) {
-        dfs(i) ;
-        ++ans ;
+  
+  // Printing connected component
+  cout<<connected_component.size()<<"\n" ;
+
+  for(auto u:connected_component) {
+    for(auto vertex:u) {
+      cout<<vertex<<' ';
     }
+    cout<<"\n";
   }
-  cout<<"Connected Components = "<<ans<<'\n' ;
+
 }

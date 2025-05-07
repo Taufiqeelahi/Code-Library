9b2bff85ea6f0262bb6bda31e8b0e898aab100b2
Taufiqeelahi <1811taufiqeelahi@gmail.com>
2025-05-07 17:21:52 +0600

    Bit mask modified

--- BitWise/BIT_MASK.cpp
+++ BitWise/BIT_MASK.cpp
@@ -3,19 +3,17 @@
 using namespace std; 
 
 int main() {
-    int n ;
-    cin>>n ;
+    int n ; cin>>n ;
 
-    int arr[n] ;
-
+    vector<int> v(n) ;
     for(int i = 0 ; i<n ; i++) {
-        cin>>arr[i] ;
+        cin>>v[i] ;
     }
 
     for(int mask = 0 ; mask<=(1<<n) ;mask++) {
         for(int i =0 ; i<n; i++)  {
             if((mask>>i)&1)  {
-                cout<<" "<<arr[i]<<" " ;
+                cout<<v[i]<<" " ;
             }
         }
         cout<<endl ;
